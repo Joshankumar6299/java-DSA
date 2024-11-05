b8fbb3d820bdd56c2de9fd5df9cbaaaa224041ed
Joshan Kumar Kushawaha <167081371+Joshankumar6299@users.noreply.github.com>
2024-11-05 21:48:54 +0530

    add input

--- input.java
+++ input.java
@@ -3,9 +3,16 @@ import java.util.*;
 public class input {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your name: "); // Prompt before reading input
-        String name = sc.next();
-        System.out.println("Your name is: " + name);
-        sc.close(); // Close the scanner
+        // System.out.print("Enter your name: "); // Prompt before reading input
+        // String name = sc.next();
+        // System.out.println("Your name is: " + name);
+        // sc.close(); // Close the scanner/
+
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int sum = a + b;
+        System.out.println("Sum is: " + sum);
     }
 }
