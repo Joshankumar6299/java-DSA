ce6a47a919590ade32ce8ebc154d117d41444f5c
joshankumar6299 <joshankumar6299@gmail.com>
2025-01-03 23:17:39 +0530

    add Solid_Rectangle class to print a solid rectangle pattern

---
+++ Solid_Rectangle.java
@@ -0,0 +1,23 @@
+public class Solid_Rectangle {
+
+public static void main(String[] args) {
+    // Q1 Solid Rectangle
+    // *****
+    // *****
+    // *****
+    // *****
+    int n = 4;
+    int m = 5;
+    for(int i = 1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+
+  
+    }
+   
+
+}
